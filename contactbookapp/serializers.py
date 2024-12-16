7d6841d501a1aefb922a830cdf0fea2e8c036ffc
Vaddi Harshitha <107269932+VaddiHarshitha@users.noreply.github.com>
2024-12-16 14:30:05 +0530

    Delete contactbookapp directory

--- contactbookapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import contacts
-
-class Contactserializer(serializers.ModelSerializer):
-    class Meta:
-        model=contacts
-        fields="__all__"
-        
